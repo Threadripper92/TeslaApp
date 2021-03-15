4de59ebde2022a9bfab16ff74e5f5d76723310ff
LAPTOP-4TN4332f <bestine1234@gmail.com>
2021-03-15 15:04:55 +0530

    added props for CarItem
    Now the background image of cars +name and tagline will change dynamically

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -6,20 +6,26 @@ import CustomButton from '../Custombutton';
 
 
 const CarItem = (props)=>{
+
+  const {name,tagLine,image,tagLineCTA} = props; 
     return(
     <View style={styles.carContainer}>
     
         <ImageBackground 
           style={styles.image} 
-          source={require('../../assets/images/ModelS.jpeg')}>
+          source={image}>
         </ImageBackground>
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at 69,000$</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{tagLine} 
+        {''}
+        <Text style={styles.subtitleCTA}>{tagLineCTA}</Text></Text>
         
     </View> 
+    <View style={styles.buttonContainer}>
     <CustomButton type="primary" content={"CUSTOM ORDER"} onPress={()=>{console.warn('Custom order was pressed')}}/>
     <CustomButton type="Secondary" content={"EXISTING INVENTORY"} onPress={()=>{console.warn('EXISTING INVENTORY was pressed')}}/>
+    </View>
     </View>
     
     );
